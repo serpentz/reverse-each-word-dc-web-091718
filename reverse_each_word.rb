b94542a2f196b24a32ed13976b4bc1a78122d229
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 15:26:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,9 @@ def reverse_each_word(string)
  array = string.split(" ")
  array.map {|word|
    word_array = word.split
-   for(counter = 0; counter<(word_array.length/2); counter++)
-      temp = word_array[counter]
+   (0..word_array.length/2).each do {|counter|      temp = word_array[counter]
       word_array[counter] = word_array[word_array.length - counter]
-   end
+   }
  }
 
 end
