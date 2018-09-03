d7db788e022b7960a82e44974afecb7792240a80
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 15:33:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 def reverse_each_word(string)
 array = string.split(" ")
-array.map { | word |
+array.map  | word |
     word_array = word.split (0..word_array.size).each do  |counter| temp = word_array[counter]
       word_array[counter] = word_array[word_array.length - counter]
       binding.pry
 
-}
+end
 end
 
 reverse_each_word("Hi again, just making sure it's reversed!")
